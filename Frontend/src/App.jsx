61ee43546392bfbe9a9151418e9a387dbf8d3b0e
Tallapally Sunny Sachin <thallapellisunny684@gmail.com>
2026-06-30 17:59:30 +0530

    Complete Expense Tracker Milestone1

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,29 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [expenses, setExpenses] = useState([])
+
+  useEffect(()=>{
+    getExpenses()
+  },[])
+
+  const getExpenses = async()=>{
+    const response = await axios.get('http://localhost:5001/expenses')
+    setExpenses(response.data)
+  }
 
   return (
     <>
-    
+    <h1>Expense Tracker</h1>
+    {expenses.map((expense)=>(
+      <div key={expense._id}>
+        <h2>{expense.title}</h2>
+        <p>{expense.amount}</p>
+        <p>{expense.category}</p>
+        <hr />
+      </div>
+    ))}
     </>
   )
 }
